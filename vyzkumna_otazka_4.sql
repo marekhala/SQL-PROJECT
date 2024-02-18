d322965243343093ad0f7f8db393a249c165bc35
marekhala <153138933+marekhala@users.noreply.github.com>
2024-02-18 14:05:03 +0100

    Add files via upload

---
+++ vyzkumna_otazka_4.sql
@@ -0,0 +1,54 @@
+-- Existuje rok, ve kterém byl meziroční nárůst cen potravin výrazně vyšší než růst mezd (větší než 10 %)?
+
+SELECT
+	price_table.common_year,
+	price_table.price_change,
+	payroll_table.payroll_change,
+	price_table.price_change - payroll_table.payroll_change AS difference
+FROM(
+WITH previous_year_price_CTE AS(
+SELECT
+	common_year,
+	avg_price_per_year,
+	LAG(avg_price_per_year) OVER (PARTITION BY 1 ORDER BY common_year) AS avg_price_previous_year
+FROM(
+SELECT 
+	common_year, 
+	ROUND(AVG(price),2) AS avg_price_per_year
+FROM t_marek_hala_project_sql_primary_final tmhpspf
+GROUP BY common_year
+) AS prices
+)
+SELECT 
+	common_year,
+	avg_price_per_year,
+	avg_price_previous_year,
+	ROUND(((avg_price_per_year-avg_price_previous_year)/avg_price_previous_year)*100,2) AS price_change
+FROM previous_year_price_CTE
+WHERE avg_price_previous_year IS NOT NULL) AS price_table
+INNER JOIN (
+WITH previous_year_payroll_CTE AS(
+SELECT
+	common_year,
+	avg_payroll,
+	LAG(avg_payroll) OVER (PARTITION BY 1 ORDER BY common_year) AS avg_payroll_previous_year
+FROM( 
+SELECT 
+	common_year, 
+	ROUND(AVG(avg_payroll),2) AS avg_payroll
+FROM t_marek_hala_project_sql_primary_final tmhpspf
+GROUP BY common_year
+) AS payroll )
+SELECT 
+	common_year,
+	avg_payroll,
+	avg_payroll_previous_year,
+	ROUND(((avg_payroll-avg_payroll_previous_year)/avg_payroll_previous_year)*100,2) AS payroll_change
+FROM previous_year_payroll_CTE
+WHERE avg_payroll_previous_year IS NOT NULL) AS payroll_table
+ON price_table.common_year = payroll_table.common_year
+ORDER BY difference DESC;
+
+
+
+ 
